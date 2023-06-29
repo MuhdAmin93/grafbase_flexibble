31df4815556d910a0b4de921a6a8d4492c81adb2
Mohammad Al-amin Idris <al-amin@Mohammads-MacBook-Air.local>
2023-06-29 09:10:45 +0300

    first commit

---
+++ flexibble/app/page.tsx
@@ -0,0 +1,15 @@
+// dependencies for this project u need to download npm install @headlessui/react cloudinary jsonwebtoken @types/jsonwebtoken graphql-request next-auth
+
+
+const Home = () => {
+  return (
+    <section className="flex-start paddings mb-16 flex-col ">
+      Categories
+      Posts
+      LoadMore
+    </section>
+  )
+}
+
+
+export default Home
